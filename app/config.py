5d797e6ad13c806a64546a8234c55ce627f92f8f
susant <gracysusant@gmail.com>
2024-02-22 18:38:48 +0545

    refactor

--- app/config.py
+++ app/config.py
@@ -1,14 +1,13 @@
-from pydantic.env_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str
-    admin_email: str
-    db_url: str
+    app_name: str = 'Scrapper API'
+    admin_email: str = 'techbizznepal@gmail.com'
+    db_url: str = 'mysql://rro@sdf'
     scrap_api_token: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
